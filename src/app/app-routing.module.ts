a1908b3390c6a73d3501d13fe91c3264107fc66e
akshay-webdev <webdev.akshay@gmail.com>
2023-01-01 02:51:55 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './componets/cart/cart.component';
+import { HomeComponent } from './componets/home/home.component';
+import { LoginComponent } from './componets/login/login.component';
+import { SellerAuthComponent } from './componets/seller-auth/seller-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path:'',
+    pathMatch:'prefix'
+  },
+  {
+    component:LoginComponent,
+    path:'login',
+  },
+  {
+    component:CartComponent,
+    path:'cart',
+  },
+  {
+    component:SellerAuthComponent,
+    path:'seller-auth',
+    pathMatch:'prefix'
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { HomeComponent } from './componets/home/home.component';
+import { LoginComponent } from './componets/login/login.component';
+import { SellerAuthComponent } from './componets/seller-auth/seller-auth.component';
+import { CartComponent } from './componets/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    SellerAuthComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
